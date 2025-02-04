4c3e08c7778c78fdd9d62f2b0232581ff31b4911
Saad <primecoder8@gmail.com>
2025-02-04 21:35:01 +0530

    Add SVG templates for various documents and integrate TemplateGallery in Home component frontend done

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./navbar"
+import TemplateGallery from "./template-gallery"
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
+      <div className="mt-16">
+        <TemplateGallery />
+      </div>  
     </div>
   ) 
 }
