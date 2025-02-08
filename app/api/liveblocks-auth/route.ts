aab61a9f8702d9dba6f7ee10454339bb983e880d
codingbot24-s <primecoder8@gmail.com>
2025-02-08 16:18:45 +0530

    Enhance user fetching and display in Room component; implement user retrieval logic, improve user resolution for mentions, and update loading state with a fullscreen loader

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -48,7 +48,7 @@ export async function POST(req: NextRequest) {
 
     const session = liveblocks.prepareSession(user.id,{
        userInfo : {
-           name : user.fullName ?? "Anonymous",
+           name : user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
            avatar : user.imageUrl,
        } 
     });
--- app/document/[documentid]/Room.tsx
+++ app/document/[documentid]/Room.tsx
@@ -1,25 +1,68 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import FullscreenLoader from "@/components/ui/fullscreen-loader";
+import { useState } from "react";
+import { useMemo } from "react";
+import { getUsers } from "./action";
+import { toast } from "sonner";
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 
 export function Room({ children }: { children: ReactNode }) {
-
   const params = useParams();
+  const [user, setUser] = useState<User[]>([]);
+
+  const fetchUser = useMemo(
+    () => async () => {
+      try {
+        const list = await getUsers();
+        setUser(list);
+      } catch {
+        toast.error("Error fetching user");
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
 
   return (
     // TODO add publicApiKey when using liveblocks
-    <LiveblocksProvider 
+    <LiveblocksProvider
+      
       throttle={16}
       authEndpoint={"/api/liveblocks-auth"}
+      resolveUsers={({userIds}) => {
+        return userIds.map((id) => user.find((user) => user.id === id)) ?? undefined;
+      }}
+      resolveMentionSuggestions={({text}) => {
+        let filteredUsers = user;
+
+        if (text) {
+          filteredUsers = user.filter((user) => user.name.toLowerCase().includes(text.toLowerCase()));
+        }
+
+        return filteredUsers.map((user) => user.id);
+      }}
+      resolveRoomsInfo={() => []}
     >
       <RoomProvider id={params.documentid as string}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <ClientSideSuspense
+          fallback={<FullscreenLoader label=" Room loading..." />}
+        >
           {children}
         </ClientSideSuspense>
       </RoomProvider>
